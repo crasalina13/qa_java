dcdf5bc402aa5927e2ba98ee58c6f431a0674921
Alina Krasnova <allina.krasnova@yandex.ru>
2025-01-08 22:48:34 +0300

    done final project in sprint 6

---
+++ src/test/java/LionNotParameterizedTest.java
@@ -0,0 +1,38 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionNotParameterizedTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testDivisionDoesHaveManeThrowsException() throws Exception {
+        Exception exception = Assert.assertThrows(Exception.class, () -> new Lion("Вертолет", feline));
+        Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+    }
+
+    @Test
+    public void testGetKittens() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        int expectedCount = 0;
+        int actualCount = lion.getKittens();
+        Assert.assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Lion lion = new Lion("Самка", feline);
+        List<String> expectedFood = List.of();
+        List<String> actualFood = lion.getFood();
+        Assert.assertEquals(expectedFood, actualFood);
+    }
+}
